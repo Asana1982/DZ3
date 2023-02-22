8dd4c01b4b258177ef27fdb8cb397a7200cbc118
Asanovich <asansvetlana@yandex.ru>
2023-02-22 14:45:40 +0300

    Массив с рандомными числами с выводом кол-ва четных чисел в нем

--- Zadacha34/Program.cs
+++ Zadacha34/Program.cs
@@ -6,7 +6,7 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1, 11); // [1, 10]
+    array[i] = new Random().Next(100, 1000); 
 
 Console.WriteLine($"[{string.Join(", ", array)}]");
 int count = 0;
