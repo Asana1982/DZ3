548018b44efb8c161b40aa66fee7defdc3698b63
Asanovich <asansvetlana@yandex.ru>
2023-02-22 13:22:32 +0300

    возведение числа в степень через цикл

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int j = 1;
+for (int i = 1; i <= n; i++)
+{
+    j = j * num; 
+}
+Console.WriteLine(j);
