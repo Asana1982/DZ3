5ef96e5ed91c427b4d12f0c81090e2871f9f15c9
Asanovich <asansvetlana@yandex.ru>
2023-03-06 22:00:27 +0300

    Задача на подсчет количества положительных введенных чисел

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.Write("Введите нужное кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine()),count = 0;
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+	    count ++;
+}	
+Console.WriteLine($"Количество положительных чисел: {count}");
