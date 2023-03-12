25894a948638b694ea275bee927e4e36b6178f48
Asanovich <asansvetlana@yandex.ru>
2023-03-12 17:45:50 +0300

    Замена строк на столбцы в двумерном массиве. Сообщение об ошибке, если кол-во строк не соответствует кол-ву столбцов

---
+++ Zadacha55/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 55. Задайте двумерный массив. Напишите программу, которая заменяет
+// строки на столбцы. В случае, если это невозможно, программа должна
+// вывести сообщение для пользователя.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 21); // [1, 20]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void ReplaceRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+while (size[0] != size[1])
+{
+    Console.Write("Вы ошиблись!\nВведите размер массива: ");
+    size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив:");
+PrintMatrix(matrix);
+ReplaceRow(matrix);
+Console.WriteLine("Конечный массив:");
+PrintMatrix(matrix);
