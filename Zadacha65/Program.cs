778a80a6e25cb5f1e81e506b229b8ef9309b9ee7
Asanovich <asansvetlana@yandex.ru>
2023-03-13 21:39:14 +0300

    Через рекурсию находим натуральные числа в промежутке двух элементов

---
+++ Zadacha65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 65. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string rec(int n, int i)
+
+{
+    if (i == n)
+        return $"{n}";
+    return rec(n, i - 1) + $", {i}";
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, i));
