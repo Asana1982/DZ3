7fece8e4a1a74ae4adca3cb93b5475766c34a983
Asanovich <asansvetlana@yandex.ru>
2023-03-13 22:30:06 +0300

    Нахождение через рекурсию суммы элементов в промежутке от M до N

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int rec(int m, int n)
+
+{
+    if (n == m)
+        return m;
+    return rec(m, n - 1) + n;
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
+
+
