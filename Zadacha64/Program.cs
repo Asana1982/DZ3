11dade78e8b6f0e47490645cb54fe05a9c49c67a
Asanovich <asansvetlana@yandex.ru>
2023-03-13 22:00:26 +0300

    Вывод через рекурсию всех натуральных чисел от N до 1

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+string rec(int n)
+
+{
+    if (n == 1)
+        return $"{1}";
+    return $"{n}, " + rec(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
